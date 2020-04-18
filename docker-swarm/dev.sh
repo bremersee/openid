26f83d1c7dc46bf1213db3dee72accf0ea9190d8
Christian Bremer <bremersee@googlemail.com>
2020-04-18 19:18:33 +0200

    Init

--- docker-swarm/dev.sh
+++ docker-swarm/dev.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
-# Clustering see:
-# - https://www.keycloak.org/2019/08/keycloak-jdbc-ping
-# - https://www.techrunnr.com/keycloak-cluster-using-docker-swarm/
-# - https://github.com/fit2anything/keycloak-cluster-setup-and-configuration
 docker service create \
-  --replicas 2 \
+  --replicas 1 \
   --name keycloak \
   --network proxy \
   --publish=58880:8080 \
@@ -26,12 +22,9 @@ docker service create \
   --env 'DB_USER_FILE=/run/secrets/keycloak_db_user' \
   --env 'DB_PASSWORD_FILE=/run/secrets/keycloak_db_password' \
   --env 'PROXY_ADDRESS_FORWARDING=true' \
-  --env 'KEYCLOAK_HOSTNAME=openid.bremersee.org' \
+  --env 'KEYCLOAK_HOSTNAME=openid.dev.bremersee.org' \
   --env 'KEYCLOAK_HTTP_PORT=80' \
   --env 'KEYCLOAK_HTTPS_PORT=443' \
   --env 'KEYCLOAK_LOGLEVEL=INFO' \
   --env 'ROOT_LOGLEVEL=INFO' \
-  --env 'JGROUPS_DISCOVERY_PROTOCOL=JDBC_PING' \
-  --env 'JGROUPS_DISCOVERY_PROPERTIES: datasource_jndi_name=java:jboss/datasources/KeycloakDS' \
-  --env 'CACHE_OWNERS_COUNT=2' \
-  bremersee/openid:latest
+  bremersee/openid:snapshot
